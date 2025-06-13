3f9a9e6b1efb8bc464c08ca4a59c1e2ce9612b3f
Jeļizaveta Vetaškova <s24vetajeli@venta.lv>
2025-06-13 20:19:32 +0300

    Pievienoti repo Kursa datumu un Sertifikatu klasem

---
+++ src/main/java/lv/sis/repo/SertifikatiRepo.java
@@ -0,0 +1,9 @@
+package lv.sis.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.sis.model.Sertifikati;
+
+public interface SertifikatiRepo extends CrudRepository<Sertifikati, Integer> {
+
+}
