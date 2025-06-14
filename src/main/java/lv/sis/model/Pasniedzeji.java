e4ab887a4caf2e9646e92322464e180ee3754dcf
Sintija Ernstreite <sintija.ernstreite@gmail.com>
2025-06-14 19:19:00 +0300

    izveidotas klases pasniedzeji un kurss

---
+++ src/main/java/lv/sis/model/Pasniedzeji.java
@@ -0,0 +1,5 @@
+package lv.sis.model;
+
+public class Pasniedzeji {
+
+}
---
+++ src/main/java/lv/sis/model/enums/Limeni.java
@@ -0,0 +1,17 @@
+package lv.sis.model.enums;
+
+public enum Limeni {
+	
+	Beginner,
+	
+	Junior,
+	
+	Intermediate,
+	
+	Advanced,
+	
+	Iesasecju_limenis, 
+	
+	
+
+}
