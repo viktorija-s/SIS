a64d8711273e83b881869f4c9fec8d951ae51a10
GKravale <gunakravale@gmail.com>
2025-10-11 00:23:10 +0300

    Added phoneNo validation for creating a professor/Changes to phoneNo variable name

--- src/main/java/lv/sis/repo/ICRUDPasniedzejiRepo.java
+++ src/main/java/lv/sis/repo/ICRUDPasniedzejiRepo.java
@@ -5,6 +5,8 @@ import lv.sis.model.Pasniedzeji;
 
 public interface ICRUDPasniedzejiRepo extends CrudRepository<Pasniedzeji, Integer>{
 
+	boolean existsByTelefonaNr(String telefonaNr);
+
 	boolean existsByVardsAndUzvards(String vards, String uzvards);
 
 	boolean existsByEpasts(String epasts);
