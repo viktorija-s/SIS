1b89678045a5dab356d81faf1b60163f1bd84d88
martHaza <martazapacka17@gmail.com>
2025-10-11 13:41:52 +0300

    Izveidots KursaDatumi service

--- src/main/java/lv/sis/service/ICRUDKursaDatumiService.java
+++ src/main/java/lv/sis/service/ICRUDKursaDatumiService.java
@@ -1,15 +1,35 @@
 package lv.sis.service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import lv.sis.model.KursaDatumi;
+import lv.sis.model.Kurss;
+import lv.sis.model.Pasniedzeji;
+
 
 public interface ICRUDKursaDatumiService {
 
-	ArrayList<KursaDatumi> retrieveAll();
-
-	KursaDatumi retrieveById(Integer id);
-
-	void deleteById(int id);
+//	ArrayList<KursaDatumi> retrieveAll();
+//
+//	KursaDatumi retrieveById(Integer id);
+//
+//	void deleteById(int id);
+	
+	//C - create
+	public abstract void create(LocalDate sakumaDatums, LocalDate beiguDatums, 
+			Kurss kurss, Pasniedzeji pasniedzejs) throws Exception;
+		
+	//R - retrieve all
+	public abstract ArrayList<KursaDatumi> retrieveAll() throws Exception;
+			
+	//R - retrieve by id
+	public abstract KursaDatumi retrieveById(int kursaDatId) throws Exception;
+			
+	//U - update
+	public abstract void updateById(int kursaDatId, KursaDatumi kursaDatumi) throws Exception;
+			
+	//D - delete
+	public abstract void deleteById(int kursaDatId) throws Exception;
 
 }
