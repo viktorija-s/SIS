5a2959b3285679396515aaeb490a6584a1f372f4
GKravale <gunakravale@gmail.com>
2025-10-12 20:15:23 +0300

    Added MyAuthority and MyUser classes

---
+++ src/main/java/lv/sis/model/MyAuthority.java
@@ -0,0 +1,54 @@
+package lv.sis.model;
+
+import java.util.Collection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "MyAuthorityTable")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class MyAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "AId")
+    @Setter(value = AccessLevel.NONE)
+    private int aId;
+
+    @NotNull
+    @Size(min = 3, max = 10)
+    @Pattern(regexp = "[A-Z_]+")
+    @Column(name = "Title")
+    private String title;
+
+    // one to many
+
+    @OneToMany(mappedBy = "authority", fetch = FetchType.EAGER)
+    @ToString.Exclude
+    private Collection<MyUser> users;
+
+    public MyAuthority(String title) {
+        this.title = title;
+    }
+
+
+
+}
