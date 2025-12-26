9517afaf2ad410d647522710461b099c0d46174c
Jeļizaveta Vetaškova <s24vetajeli@venta.lv>
2025-12-26 17:05:24 +0200

    Added pagination to KursaDatumi

--- src/main/java/lv/sis/repo/IKursaDatumiRepo.java
+++ src/main/java/lv/sis/repo/IKursaDatumiRepo.java
@@ -1,13 +1,15 @@
 package lv.sis.repo;
 
-import java.util.ArrayList;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 
 import lv.sis.model.KursaDatumi;
 
 public interface IKursaDatumiRepo extends CrudRepository<KursaDatumi, Integer> {
 
-	ArrayList<KursaDatumi> findAllByPasniedzejsPid(int pid);
+	Page<KursaDatumi> findAll(Pageable pageable);
+
+	Page<KursaDatumi> findAllByPasniedzejsPid(int pid, Pageable pageable);
 
 }
